bc07324b94914920a340f05be9bf6919a2d47d82
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-02-10 22:10:29 +0530

    Added practiced SI

---
+++ Hollow_Rect.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int m,n;
+    scanf("%d",&m);
+    scanf("%d",&n);
+    int a[n][m];
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
+            if(i==0 || j==0 || i==n-1 || j==m-1) {
+                printf("*");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }  
+    return 0;
+}
