8cbca721427fc082e1b464eb2f72745955a7862c
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-02-10 22:10:14 +0530

    Added practiced SI

---
+++ First_Last.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++) {
+        scanf("%d",&a[i]);
+    }   
+    int x;
+    scanf("%d",&x);
+    int first=-1,last=-1;
+    for(int i=0;i<n;i++) {
+        if(a[i]==x) {
+            if(first==-1) {
+                first=i;
+            }
+            last=i;
+        }
+    }
+    printf("%d %d",first,last);
+    return 0;
+}
