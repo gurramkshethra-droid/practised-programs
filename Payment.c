a350b7bad242f862133d86df2b678c46c5f8cbf2
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-02-11 21:03:45 +0530

    Added practiced codechef

---
+++ Payment.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int n;
+	scanf("%d",&n);
+	int m=100+(n*4);
+	if(m>300) {
+	    printf("300");
+	}
+	else {
+	    printf("%d",m);
+	}
+
+}
+
