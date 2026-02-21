e90a6a98fd1f53eead03bac5738e824b28f0e551
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-02-21 17:18:09 +0530

    Added SI practised

--- Matrix_sum.c
+++ Matrix_sum.c
@@ -2,19 +2,20 @@
 
 int main() {
 
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
-    int n,m;
-    scanf("%d",&n);
-    scanf("%d",&m);
-    int a[n][m];
-    for(int i=0;i<n;i++) {
-        int rowsum=0;
-        for(int j=0;j<m;j++) {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    for (int i = 0; i < n; i++) {
+        int rowsum = 0;
+
+        for (int j = 0; j < m; j++) {
             int x;
-            scanf("%d",&x);
-            rowsum+=x;
+            scanf("%d", &x);
+            rowsum += x;
         }
-        printf("%d\n",rowsum);
+
+        printf("%d\n", rowsum);
     }
+
     return 0;
 }
