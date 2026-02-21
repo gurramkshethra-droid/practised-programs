49d4d3d2a495abb4120ae077b52d36a3f4e34983
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-02-21 17:18:20 +0530

    Added SI practised

--- duplicate.c
+++ duplicate.c
@@ -2,20 +2,26 @@
 
 int main() {
 
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     int n;
-    scanf("%d",&n);
-    int a[100];
-    for(int i=0;i<n;i++) {
-        scanf("%d",&a[i]);
-    } 
-    for(int i=0;i<n;i++) {
-        for(int j=i+1;j<n;j++) {
-            if(a[i]==a[j]) {
-                printf("%d",a[i]);
+    scanf("%d", &n);
+
+    int a[n];   // dynamic size (C99)
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] == a[j]) {
+                printf("%d\n", a[i]);
                 return 0;
             }
         }
     }
+
+    // If no duplicate found
+    printf("No duplicate\n");
+
     return 0;
 }
