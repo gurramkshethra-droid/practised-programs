59116405bbd081a9113383baa3d2b467e4de4808
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-02-28 22:12:39 +0530

    Added

--- bubble.c
+++ bubble.c
@@ -2,25 +2,40 @@
 
 int main() {
 
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
-    int n,temp=0;
-    scanf("%d",&n);
-    int a[25];
-    for(int i=0;i<n;i++) {
-        scanf("%d",&a[i]);
-    } 
-    for(int i=0;i<n-1;i++) {
-        for(int j=0;j<n-i-1;j++) {
-            if(a[j]>a[j+1]) {
-                temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
+    int n;
+    scanf("%d", &n);
+
+    int a[n];   // dynamic size array
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    // Bubble Sort with optimization
+    for(int i = 0; i < n - 1; i++) {
+
+        int swapped = 0;   // check if swap happened
+
+        for(int j = 0; j < n - i - 1; j++) {
+            if(a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+                swapped = 1;
             }
         }
-        for(int k=0;k<n;k++) {
-            printf("%d ",a[k]);
-        } 
+
+        // Print array after each pass
+        for(int k = 0; k < n; k++) {
+            printf("%d ", a[k]);
+        }
         printf("\n");
+
+        // If no swaps, array is already sorted
+        if(swapped == 0) {
+            break;
+        }
     }
+
     return 0;
 }
