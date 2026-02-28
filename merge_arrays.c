ba4704b8b105d55c7f043192ebbed5f24af0bfcc
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-02-28 22:12:56 +0530

    Added

--- merge_arrays.c
+++ merge_arrays.c
@@ -2,28 +2,37 @@
 
 int main() {
 
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    int n,m,i=0,j=0;
-    int a[1005],b[1005];
-    scanf("%d",&n);
-    for(int i=0;i<n;i++)  {
-        scanf("%d",&a[i]);
+    int n, m;
+    scanf("%d", &n);
+
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
     }
-    scanf("%d",&m);
-    for(int i=0;i<m;i++) {
-        scanf("%d",&b[i]);
+
+    scanf("%d", &m);
+
+    int b[m];
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &b[i]);
     }
-    while(i<n && j<m) {
-        if(a[i]<=b[j])
-        printf("%d ",a[i++]);
+
+    int i = 0, j = 0;
+
+    while (i < n && j < m) {
+        if (a[i] <= b[j])
+            printf("%d ", a[i++]);
         else
-        printf("%d ",b[j++]);
+            printf("%d ", b[j++]);
     }
-    while(i<n) {
-        printf("%d ",a[i++]);
-    }
-    while(j<m) {
-        printf("%d ",b[j++]);
-    }
+
+    while (i < n)
+        printf("%d ", a[i++]);
+
+    while (j < m)
+        printf("%d ", b[j++]);
+
+    printf("\n");
+
     return 0;
 }
