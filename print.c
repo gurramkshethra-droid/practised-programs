a310e8ed95306408011e10106cffabebcd4f44ca
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-02-28 22:13:29 +0530

    Added

--- print.c
+++ print.c
@@ -2,29 +2,30 @@
 
 int main() {
 
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
-    int n,m;
-    scanf("%d",&n);
-    int a[100];
-    for(int i=0;i<n;i++) {
-        scanf("%d",&a[i]);
-    }   
-    scanf("%d",&m);
-    int b[100];
-    for(int i=0;i<m;i++) {
-        scanf("%d",&b[i]);
-    } 
-    for(int i=0;i<m;i++) {
-        if(b[i]>=0 && b[i]<n) {
-            printf("%d",a[b[i]]);
-        }
-        else {
+    int n, m;
+    scanf("%d", &n);
+
+    int a[n];   // dynamic size based on input
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    scanf("%d", &m);
+
+    for(int i = 0; i < m; i++) {
+        int index;
+        scanf("%d", &index);
+
+        if(index >= 0 && index < n) {
+            printf("%d", a[index]);
+        } else {
             printf("-1");
         }
-        if(i!=m-1) {
+
+        if(i < m - 1) {
             printf(" ");
         }
     }
-    printf("\n");
+
     return 0;
 }
