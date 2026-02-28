04a5a95752d6ab58577455b25577a161598b4441
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-02-28 22:13:04 +0530

    Added

--- mmm.c
+++ mmm.c
@@ -2,38 +2,61 @@
 
 int main() {
 
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
+
     int a[n];
-    double sum=0;
-    for(int i=0;i<n;i++) {
-        scanf("%d",&a[i]);
-        sum+=a[i];
-    }   
-    double mean=sum/n;
-    double median;
-    if(n%2==0) {
-        median=(a[(n/2)-1]+a[n/2])/2.0;
+    double sum = 0;
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+        sum += a[i];
     }
-    else {
-        median=a[n/2];
-    }
-    int maxc=1,count=1,mode=a[0],i;
-    for(i=1;i<n;i++) {
-        if(a[i]==a[i-1]) {
-            count++;
-        } else {
-            if(maxc<count) {
-                maxc=count;
-                mode=a[i-1];
+
+    // 🔹 SORT THE ARRAY (Simple Bubble Sort)
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
             }
-            count=1;
         }
     }
-    if(count>maxc) {
-        mode=a[n-1];
+
+    // 🔹 MEAN
+    double mean = sum / n;
+
+    // 🔹 MEDIAN
+    double median;
+    if(n % 2 == 0) {
+        median = (a[(n/2) - 1] + a[n/2]) / 2.0;
+    } else {
+        median = a[n/2];
     }
-    printf("%.2f %.2f %d",mean,median,mode);
+
+    // 🔹 MODE
+    int maxc = 1, count = 1;
+    int mode = a[0];
+
+    for(int i = 1; i < n; i++) {
+        if(a[i] == a[i - 1]) {
+            count++;
+        } else {
+            if(count > maxc) {
+                maxc = count;
+                mode = a[i - 1];
+            }
+            count = 1;
+        }
+    }
+
+    // Check last element
+    if(count > maxc) {
+        mode = a[n - 1];
+    }
+
+    printf("%.2f %.2f %d", mean, median, mode);
+
     return 0;
 }
