276a6ce79a025872ead8e8eca2e1234ee03a9cbd
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-03-01 20:07:06 +0530

    Added

--- matrix-sum.c
+++ matrix-sum.c
@@ -2,23 +2,26 @@
 
 int main() {
 
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
-    int n,m;
-    scanf("%d",&n);
-    scanf("%d",&m);
-    int a[n][m];
-    for(int i=0;i<n;i++) {
-        for(int j=0;j<m;j++) {
-            scanf("%d",&a[i][j]);
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int a[100][100];   // safer fixed size
+
+    // Input matrix
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            scanf("%d", &a[i][j]);
         }
-    } 
-    int sum=0;
-    for(int j=0;j<m;j++) {
-        sum=0;
-        for(int i=0;i<n;i++) {
-            sum+=a[i][j];
+    }
+
+    // Column-wise sum
+    for(int j = 0; j < m; j++) {
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            sum += a[i][j];
         }
-        printf("%d\n",sum);
-    } 
+        printf("%d\n", sum);
+    }
+
     return 0;
 }
