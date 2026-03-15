964246e33efec69347299744d97823b8a28f3084
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-03-15 23:43:51 +0530

    hollow rectangle
    Added a comment to clarify the condition for printing '*' in the hollow rectangle.

--- Hollow_Rect.c
+++ Hollow_Rect.c
@@ -9,7 +9,7 @@ int main() {
     int a[n][m];
     for(int i=0;i<n;i++) {
         for(int j=0;j<m;j++) {
-            if(i==0 || j==0 || i==n-1 || j==m-1) {
+            if(i==0 || j==0 || i==n-1 || j==m-1) {//condition
                 printf("*");
             }
             else {
