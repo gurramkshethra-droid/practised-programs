ce6c0f2977b53f77a8996847e0007e92fad486ba
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-03-23 18:38:29 +0530

    Added si practiced

---
+++ Dup.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Dup {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+        int n;
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++) {
+            a[i]=s.nextInt();
+        }
+        for(int i=0;i<n;i++)  {
+            for(int j=i+1;j<n;j++) {
+                if(a[i]==a[j]) {
+                    System.out.print(a[i]);
+                    return;
+                }
+            }
+        }
+    }
+}
