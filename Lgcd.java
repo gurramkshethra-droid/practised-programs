7aa0fc6174ac76a4391abf795587103a3f686020
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-03-23 18:39:09 +0530

    Added si practiced

---
+++ Lgcd.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Lgcd {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+        int m,n;
+        Scanner s=new Scanner(System.in);
+        m=s.nextInt();
+        n=s.nextInt();
+        int gcd1=gcd(m,n);
+        int lcm=(m*n)/gcd1;
+        System.out.print("LCM is "+lcm+", GCD is "+gcd1);
+    }
+    public static int gcd(int m,int n) {
+        int r;
+        while(n!=0) {
+            r=m%n;
+            m=n;
+            n=r;
+        }
+        return m;
+    }
+}
