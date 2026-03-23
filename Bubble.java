d8ccdf8718cdb8460e2bfe3c8a59caa02ac53fda
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-03-23 18:38:03 +0530

    Added si practiced

---
+++ Bubble.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Bubble {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+        int n;
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++) {
+            a[i]=s.nextInt();
+        }
+        int temp;
+        for(int i=0;i<n-1;i++) {
+            for(int j=0;j<n-1-i;j++) {
+                if(a[j]>a[j+1]) {
+                    temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+            for(int k=0;k<n;k++) {
+            System.out.print(a[k]+" ");
+        }
+        System.out.println();
+        }
+    }
+}
