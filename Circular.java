9bd855c02367a4d01b30e59e160982c06bbbe6ec
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-03-24 20:09:41 +0530

    Remove placeholder comment in Circular.java
    Removed placeholder comment for code implementation

--- Circular.java
+++ Circular.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Circular {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
         int n,k;
         Scanner s=new Scanner(System.in);
         n=s.nextInt();
