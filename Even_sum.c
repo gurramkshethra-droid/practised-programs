cfaa6ba7da56a2dd1ab1efdd3826432ae622b94f
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-03-24 19:56:00 +0530

    Add standard libraries to Even_sum.c

--- Even_sum.c
+++ Even_sum.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-int main() {\
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() {
 	int n;
 	scanf("%d",&n);
 	for(int i=0;i<n;i++) {
