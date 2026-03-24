ff7c6af4c65ea1f2c16b4e7b569ce5049e67df48
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-03-24 19:50:58 +0530

    Implement Excel column to number conversion

---
+++ ExcelColumnToNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        int result = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            result = result * 26 + (str.charAt(i) - 'A' + 1);
+        }
+
+        System.out.println(result);
+    }
+}
