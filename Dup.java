e17629b85677006e98edef5c3e1e1d1a160716da
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-03-24 19:50:01 +0530

    Remove placeholder comment in Dup.java
    Removed placeholder comment for code implementation.

--- Dup.java
+++ Dup.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Dup {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
         int n;
         Scanner s=new Scanner(System.in);
         n=s.nextInt();
