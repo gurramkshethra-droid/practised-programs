1b5b2a3ecd4687944804392f8c460ca42e9585c9
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-03-30 23:36:20 +0530

    Add MatrixZigZagTraversal class
    Implement zigzag traversal of a matrix.

---
+++ MatrixZigZagTraversal.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int m,n,i,j;
+        Scanner sc= new Scanner(System.in);
+        m=sc.nextInt();
+        n=sc.nextInt();
+        int [][]a=new int[m][n];
+        for(i=0;i<m;i++){
+            for(j=0;j<n;j++){
+                a[i][j]=sc.nextInt();
+            }
+        }
+        for(i=0;i<m;i++){
+            if(i%2==0){
+                for(j=0;j<n;j++){
+                    System.out.print(a[i][j]+" ");
+                }
+            }
+                else {
+                    for(j=n-1;j>=0;j--){
+                        System.out.print(a[i][j]+" ");
+                    }
+                }
+                
+            
+        }
+
+
+        
+    }
+}
