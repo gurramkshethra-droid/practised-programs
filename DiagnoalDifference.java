a3be86e161322ab5ec594615b262a62acc406bf9
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-03-30 23:38:54 +0530

    Create DiagnoalDifference.java

---
+++ DiagnoalDifference.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int m,n,i,j;
+        int value;
+        Scanner sc= new Scanner(System.in);
+        n=sc.nextInt();
+        int sum1=0,sum2=0;
+            for(i=0;i<n;i++){
+                for(j=0;j<n;j++){
+                    value=sc.nextInt();
+                    if(i==j){
+                        sum1+=value;
+                    }
+                    if(i+j==n-1){
+                        sum2+=value;
+                    }
+
+                }
+            }
+            System.out.println(Math.abs(sum1-sum2));
+
+    }
+}
