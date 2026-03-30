163ae93ce648e795501af1243131075550a37059
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-03-30 23:35:38 +0530

    Add Lower Triangle Sum Calculation in Lowtriangle.java
    Implement a program to calculate the sum of values in a lower triangle of a matrix.

---
+++ Lowtriangle.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int m,n,i,j;
+        Scanner sc= new Scanner(System.in);
+        n=sc.nextInt();
+        int value;
+        int sum=0;
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                value=sc.nextInt();
+                if(i>=j){
+                    sum+=value;
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
