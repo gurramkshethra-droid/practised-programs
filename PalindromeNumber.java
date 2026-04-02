4c574c7d023bb906e87c8a50f223a4e70c52c567
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-04-02 22:35:45 +0530

    Implement palindrome check in PalindromeNumber.java

---
+++ PalindromeNumber.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static boolean isPalindrome(int x) {
+        
+        // Negative numbers are not palindrome
+        if (x < 0) return false;
+
+        int original = x;
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+
+    public static void main(String[] args) {
+        int x = 121;
+        System.out.println(isPalindrome(x));
+    }
+}
