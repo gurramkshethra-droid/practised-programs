32aba0e28d07c0517cd64151e7a9b507b20c1dee
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-04-03 23:47:39 +0530

    Add imports for I/O and utility classes

--- IntegertoRoman.java
+++ IntegertoRoman.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
 class Solution {
     public String intToRoman(int num) {
         int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
