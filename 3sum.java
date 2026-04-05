905ff11c92b0c92741d694b4b715b3759b9105d7
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-04-05 21:46:27 +0530

    Remove duplicate skipping comments from 3sum.java
    Removed comments that skip duplicates in the 3Sum algorithm.

--- 3sum.java
+++ 3sum.java
@@ -7,8 +7,6 @@ class Solution {
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
-            
-            // Skip duplicates
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
             int left = i + 1;
@@ -20,7 +18,6 @@ class Solution {
                 if (sum == 0) {
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                    // Skip duplicates
                     while (left < right && nums[left] == nums[left + 1]) left++;
                     while (left < right && nums[right] == nums[right - 1]) right--;
 
