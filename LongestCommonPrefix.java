0485829526546a42c048e2314633b7bb26d2ceb7
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-04-05 21:46:03 +0530

    Add imports for I/O and utility classes

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) return "";
