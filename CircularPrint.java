5b5f920c55bfc45942f7d561f7e50637f0344ca8
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-04-06 20:49:16 +0530

    Update CircularPrint.java

--- CircularPrint.java
+++ CircularPrint.java
@@ -1,6 +1,8 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
 public class Main {
-
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int n=sc.nextInt();
         int k=sc.nextInt(),arr[]=new int[n];;
