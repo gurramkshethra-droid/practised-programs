922789593bc09187f926cf5f9548a8cbc70ccfee
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-04-12 20:16:43 +0530

    Update 4sum.java

--- 4sum.java
+++ 4sum.java
@@ -9,11 +9,9 @@ class Solution {
         Arrays.sort(nums);
         
         for (int i = 0; i < n - 3; i++) {
-            // skip duplicates
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             
             for (int j = i + 1; j < n - 2; j++) {
-                // skip duplicates
                 if (j > i + 1 && nums[j] == nums[j - 1]) continue;
                 
                 int left = j + 1;
@@ -24,8 +22,6 @@ class Solution {
                     
                     if (sum == target) {
                         result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-                        
-                        // skip duplicates
                         while (left < right && nums[left] == nums[left + 1]) left++;
                         while (left < right && nums[right] == nums[right - 1]) right--;
                         
