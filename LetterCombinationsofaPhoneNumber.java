486034a9564110f25b1164452c8b7708bf875670
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-04-12 20:19:08 +0530

    Update LetterCombinationsofaPhoneNumber.java

--- LetterCombinationsofaPhoneNumber.java
+++ LetterCombinationsofaPhoneNumber.java
@@ -1,5 +1,6 @@
+import java.io.*;
 import java.util.*;
-
+import java.util.Scanner;
 class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
