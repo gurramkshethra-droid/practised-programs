c708b92fdf0103278ade1d76265fa59f364ec1f2
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-04-17 22:25:55 +0530

    Create Longest Valid Parentheses.java

---
+++ Longest Valid Parentheses.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class LongestValidParentheses {
+
+    public static int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1); // base index
+
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    int length = i - stack.peek();
+                    maxLength = Math.max(maxLength, length);
+                }
+            }
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "(()";
+        String s2 = ")()())";
+        String s3 = "";
+
+        System.out.println(longestValidParentheses(s1)); // 2
+        System.out.println(longestValidParentheses(s2)); // 4
+        System.out.println(longestValidParentheses(s3)); // 0
+    }
+}
